57610644d72f643f1df9644f29d99c463dddbdf9
Josep <>
2023-05-02 16:32:46 +0200

    refactor files

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,6 @@ var router = express.Router();
 
 const excels = require("../lib/excel_utils")
 const readDB = require("../lib/database")
-const modelTest = require("../lib/R_test_model")
 
 /* GET home page. */
 router.get('/filtering', function(req, res, next) {
@@ -17,15 +16,5 @@ router.get('/db', async function(req, res){
   let db = await readDB.db_to_polars(records = records);
   res.send(db)
 });
-
-router.get('/modeltest', async function(req, res){
-  let model = await modelTest.modelTest()
-  res.send(model)
-});
-
-router.get('/testR', async function(req, res){
-  let test = await modelTest.testR()
-  res.send(test)
-})
 
 module.exports = router;
