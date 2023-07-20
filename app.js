71f0d3e6e6f5932f20fef4644bb2b033b0679acb
Josep <>
2023-07-20 14:05:54 +0200

    move excel delphi to public

--- app.js
+++ app.js
@@ -24,6 +24,7 @@ const treatment = require('./routes/treatment');
 
 app.use('/', index);
 app.use('/treatment', treatment)
+app.use(express.static('public'))
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
