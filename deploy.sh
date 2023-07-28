2c5861bba014e651151ea568a7d449ac067c03fa
Josep <>
2023-07-28 12:50:29 +0200

    WIP

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,20 @@
+npm run test
+if [ $? -eq 1 ]
+then
+  echo 'Some unit test failed'
+  exit 1
+fi
+
+newman run ./postman/local.json --bail --silent
+if [ $? -eq 1 ]
+then
+  echo 'Some endpoint test failed in local server'
+  exit 1
+fi
+
 echo 'Copying .env file to server...'
 scp ./.env root@icra.loading.net:/var/www/vhosts/icradev.cat/snappAPI-v2.icradev.cat/snappAPI-v2
+
 echo 'Connecting to server...'
 ssh root@icra.loading.net "
 cd /var/www/vhosts/icradev.cat/snappAPI-v2.icradev.cat/snappAPI-v2 ;
@@ -9,3 +24,4 @@ sudo -u icradev find . -type d -exec chmod 755 {} \; ;
 touch ./tmp/restart.txt ;
 echo 'Deployment done!'
 "
+newman run ./postman/server.json
--- tests/test-add-sci-study.js
+++ tests/test-add-sci-study.js
@@ -1,24 +1,28 @@
 const {expect} = require("chai")
 const {addSciStudy} = require("../lib/add-sci-study");
 const dotenv = require('dotenv')
+const {closeDB} = require("../lib/database");
 dotenv.config()
 
 describe('Test /add-sci-study', ()=> {
-    describe('Sanity checks', () =>{
+    describe('Sanity checks', () => {
         it('Returns error when body is not an object', async () => {
             let result = await addSciStudy([1, 'a'])
             expect(result).to.have.key('error')
         });
         it('Returns error when token or username is not correct', async () => {
             let result = await addSciStudy({username: "Fake", token: process.env.TOKEN_JOSPUEYO})
-            console.log(result)
             expect(result).to.have.key('error')
             result = await addSciStudy({username: process.env.TOKEN_USERNAME, token: 'Fake'})
-            console.log(result)
             expect(result).to.have.key('error')
-        });
+        })
         it('Returns error when email is not provided', () =>{
-
+            let result = 1
+        })
+    });
+    describe('Close DB connect', ()=> {
+        it('Close DB connect', () => {
+            closeDB()
         })
     });
 })
