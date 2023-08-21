2f0dbd1e6b36d5d993085c29fab9d3aa1538fd79
Josep <>
2023-08-21 16:17:02 +0200

    add-sci-study and test

--- routes/sci-studies.js
+++ routes/sci-studies.js
@@ -2,6 +2,7 @@ var express = require('express');
 var router = express.Router();
 
 const {addSciStudy} = require("../lib/add-sci-study");
+const {deleteSciStudy} = require("../lib/delete-sci-study");
 const {dbToPolars} = require("../lib/database");
 
 router.get('/sci-studies', async function(req, res){
@@ -15,7 +16,24 @@ router.get('/sci-studies', async function(req, res){
 router.post('/add-sci-study', async function(req, res){
     try {
         let result = await addSciStudy(req.body)
-        console.log(result)
+        // console.log(result)
+        if (result.error) {
+            res.statusMessage = result.error
+            res.status(400)
+            res.send(result.error)
+        } else {
+            res.send(result)
+        }
+    }
+    catch (e){
+        res.send(e)
+    }
+});
+
+router.post('/delete-sci-study', async function(req, res){
+    try {
+        let result = await deleteSciStudy(req.body)
+        // console.log(result)
         if (result.error) {
             res.statusMessage = result.error
             res.status(400)
--- tests/test-add-sci-study.js
+++ tests/test-add-sci-study.js
@@ -1,6 +1,6 @@
 const {expect} = require("chai")
 const {addSciStudy} = require("../lib/add-sci-study");
-const {closeDB} = require("../lib/database");
+const {deleteSciStudyDB, closeDB, readSciStudies} = require("../lib/database");
 const {parseDoi} = require("../lib/utils");
 const dotenv = require('dotenv')
 dotenv.config()
@@ -17,11 +17,19 @@ let body = {
         surface: 100
     },
     water: {
-        inflow: 100
+        inflow: 100,
+        type: "greywater"
     },
-    pollutants: {},
+    pollutants: {
+        bod_in: 10,
+        po43_in: 43,
+        no3_in: 3,
+        no3_out: 2
+    },
     document: {
-        doi: "10.12688/openreseurope.16054.1"
+        doi: "10.12688/openreseurope.16054.1",
+        title: "internal test",
+        year: 2020
     }
 }
 
@@ -37,7 +45,7 @@ describe('Test /add-sci-study', ()=> {
             result = await addSciStudy({username: username, token: 'Fake'})
             expect(result).to.have.key('error')
         })
-        it('Returns error when email is not provided or wrong', async () =>{
+        it('Returns error when email is not provided or wrong', async () => {
             body.email = null
             expect(await addSciStudy(body)).to.have.key('error')
             body.email = 'jpueyo[at]icra.cat'
@@ -61,15 +69,56 @@ describe('Test /add-sci-study', ()=> {
             expect(await addSciStudy(body)).to.have.key('error')
             body.technology.peopleServed = '-1'
             expect(await addSciStudy(body)).to.have.key('error')
+            body.technology.surface = 100
+            body.technology.hrt = 1
+            body.technology.peopleServed = 1000
         });
         it('Return error if water.inflow is not a positive number', async () => {
             body.water.inflow = 0
             expect(await addSciStudy(body)).to.have.key('error')
+            body.water.inflow = 100
         });
         it('Return error if water.type is not any water type', async () => {
             body.water.type = "fake"
             expect(await addSciStudy(body)).to.have.key('error')
+            body.water.type = 'greywater'
         });
+        it('Return error if out is provided but not in for pollutant concentration', async () => {
+            let old_pollutants = body.pollutants
+            body.pollutants = {bod_out: 1}
+            console.log(body.pollutants)
+            expect(await addSciStudy(body)).to.have.key('error')
+            body.pollutants = old_pollutants
+        });
+        it('Return error if year is in the future', async () => {
+            old_document = body.document
+            body.document.year = new Date().getFullYear() + 1
+            expect(await addSciStudy(body)).to.have.key('error')
+            body.document = old_document
+            body.document.year = 2020
+        });
+    });
+    describe('Insert case study', () => {
+        it('Insert case study', async () => {
+            let result = await addSciStudy(body)
+            expect(result).eq('Case study inserted')
+            let inserted = await readSciStudies()
+            inserted = inserted.filter(s => s.username === body.username)
+            expect(inserted.length).eq(1)
+            expect(inserted[0].po43_in).eq(43)
+            expect(inserted[0].no3_out).eq(2)
+            expect(inserted[0].tn_in).eq(null)
+            expect(inserted[0].type).eq("GW")
+            let document = JSON.parse(inserted[0].doc_data)
+            expect(document.title).eq(body.document.title)
+        });
+        it('Remove test insertion', async () => {
+            let result = await deleteSciStudyDB('username', 'jospueyo')
+            expect(result).eq('Case studies deleted')
+            let inserted = await readSciStudies()
+            inserted = inserted.filter(s => s.username === body.username)
+            expect(inserted.length).eq(0)
+        })
     });
     describe('Close DB connect', ()=> {
         it('Close DB connect', () => {
