dbd91267a56737ecb7acb1dba33dfc1c4d45f057
Josep <>
2023-11-22 12:04:26 +0100

    changes to deploy

--- deploy.sh
+++ deploy.sh
@@ -33,7 +33,7 @@ fi
 echo 'Connecting to server...'
 ssh root@icra.loading.net "
 cd /var/www/vhosts/icradev.cat/snappAPI-v2.icradev.cat/snappAPI-v2 ;
-sudo -u icradev git pull ;
+sudo -u icradev git pull --allow-unrelated-histories ;
 sudo -u icradev npm install ;
 sudo -u icradev find . -type f -exec chmod 644 {} \; ;
 sudo -u icradev find . -type d -exec chmod 755 {} \; ;
