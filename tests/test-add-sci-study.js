013c6341170804a4a6b57f085958ab84d75aa7cf
David Crous Mayné <crous.mayne@gmail.com>
2024-02-27 15:17:06 +0100

    Fix
    Fix tests to insert add sci-study.

--- tests/test-add-sci-study.js
+++ tests/test-add-sci-study.js
@@ -9,101 +9,64 @@ const username = process.env.TOKEN_USERNAME
 const token = process.env.TOKEN_FRONTEND
 
 let body = {
-    username: "jospueyo",
-    token: "e1fh3udlup5vprrbbpm6fq",
-    email: "jpueyo@icra.cat",
-    technology: {
-        techId: "WW",
-        surface: 100,
-        yearOperation: 2020
+    //User data is set in the auth middleware
+    user_data: {
+        id: 19
+        //email: 'crous.mayne@gmail.com',
+        //role: 'admin'
     },
-    water: {
-        inflow: 100,
-        type: "greywater"
-    },
+    id_sci_publication: 2346,
+    surface: 0,
+    hrt: 0,
+    population: 0,
+
+    //water attributes
+    water_type: 'any_wastewater',
+    uws: 'WWTP',
+    water_temp: -1,
+    air_temp: -1,
+    inflow: -1,
+
     pollutants: {
-        bod_in: 10,
-        po43_in: 43,
-        no3_in: 3,
-        no3_out: 2
-    },
-    document: {
-        doi: "10.12688/openreseurope.16054.1",
-        title: "internal test",
-        year: 2020
+        //inflow
+        bod_in: -1,
+        tn_in: -1,
+        cod_in: -1,
+        no3_in: -1,
+        nh4_in: -1,
+        tp_in: -1,
+        po43_in: -1,
+        ecoli_in: -1,
+        heggs_in: -1,
+
+        //out
+        bod_out: -1,
+        tn_out: -1,
+        cod_out: -1,
+        no3_out: -1,
+        nh4_out: -1,
+        tp_out: -1,
+        po43_out: -1,
+        ecoli_out: -1,
+        heggs_out: -1
     }
-}
+};
 
 describe('Test /add-sci-study', ()=> {
     describe('Sanity checks', () => {
         it('Returns error when body is not an object', async () => {
-            let result = await addTreatmentSciDetails([1, 'a'])
-            expect(result).to.have.key('error')
+            let result = await addTreatmentSciDetails(null)
+            expect(result).to.have.keys(['error', 'success']);
         });
-        it('Returns error when token or username is not correct', async () => {
-            let result = await addTreatmentSciDetails({username: "Fake", token: token})
-            expect(result).to.have.key('error')
-            result = await addTreatmentSciDetails({username: username, token: 'Fake'})
-            expect(result).to.have.key('error')
-        })
-        it('Returns error when email is not provided or wrong', async () => {
-            body.email = null
-            expect(await addTreatmentSciDetails(body)).to.have.key('error')
-            body.email = 'jpueyo[at]icra.cat'
-            expect(await addTreatmentSciDetails(body)).to.have.key('error')
-            body.email = 'jpueyo@icra.cat'
-        });
-        it('Validate DOI', async () => {
-            expect((await parseDoi('10.12688/openreseurope.16054.1')).startsWith("https://doi.org/")).eq(true)
-            expect(await parseDoi('https://google.com')).to.have.key('error')
-            // expect(await parseDoi('12688/openreseurope.16054.1')).to.have.key('error')
-        });
-        it('Return error if techId is not any technology', async () => {
-            body.technology.techId = "fake"
-            expect(await addTreatmentSciDetails(body)).to.have.key('error')
-            body.technology.techId = "WW"
-        });
-        it('Return error if surface, hrt or peopleServed is not a positive number', async () => {
-            body.technology.surface = 0
-            expect(await addTreatmentSciDetails(body)).to.have.key('error')
-            body.technology.hrt = 'not a number'
-            expect(await addTreatmentSciDetails(body)).to.have.key('error')
-            body.technology.peopleServed = '-1'
-            expect(await addTreatmentSciDetails(body)).to.have.key('error')
-            body.technology.surface = 100
-            body.technology.hrt = 1
-            body.technology.peopleServed = 1000
-        });
-        it('Return error if yearOperation is not between 1950 and current year', async () => {
-            body.technology.yearOperation = 1949
-            expect(await addTreatmentSciDetails(body)).to.have.key('error')
-            body.technology.yearOperation = new Date().getFullYear() + 1
-            expect(await addTreatmentSciDetails(body)).to.have.key('error')
-            body.technology.yearOperation = 2020
 
+        it('Returns error when user data is not correct', async () => {
+            let result = await addTreatmentSciDetails({})
+            expect(result).to.have.keys(['error', 'success']);
         });
-        it('Return error if water.inflow is not a positive number', async () => {
-            body.water.inflow = 0
-            expect(await addTreatmentSciDetails(body)).to.have.key('error')
-            body.water.inflow = 100
-        });
-        it('Return error if water.type is not any water type', async () => {
-            body.water.type = "fake"
-            expect(await addTreatmentSciDetails(body)).to.have.key('error')
-            body.water.type = 'greywater'
-        });
-        it('Return error if out is provided but not in for pollutant concentration', async () => {
-            let old_pollutants = body.pollutants
-            body.pollutants = {bod_out: 1}
-            expect(await addTreatmentSciDetails(body)).to.have.key('error')
-            body.pollutants = old_pollutants
-        });
-        it('Return error if year is in the future', async () => {
-            old_document = body.document
-            body.document.year = new Date().getFullYear() + 1
-            expect(await addTreatmentSciDetails(body)).to.have.key('error')
-            body.document = old_document
-            body.document.year = 2020
+
+        it('Returns error user data is not correct', async () => {
+            let result = await addTreatmentSciDetails(body);
+            expect(result).to.have.keys(['error', 'success','message','details']);
         });
     });
     // describe('Insert case study', () => {
