a75dd2cb5fa4731a9deafd9bc74beb1ce5a4c317
Josep Pueyo-Ros <jpueyo@icra.cat>
2024-09-25 15:37:51 +0200

    test in MCDA not passing

--- tests/test-mcda.js
+++ tests/test-mcda.js
@@ -41,6 +41,7 @@ describe('Test /mcda', () => {
         it('result is an array of the same length as techs', async () => {
             let selection = await findNBS({})
             let resultTechs = await mcda({techs: selection})
+            console.log("resultTechs", resultTechs)
             let resultTechIds = await mcda({techIds: ['A_HA', 'WW']})
             expect(resultTechs).to.be.an('array')
             expect(resultTechIds).to.be.an('array')
@@ -48,26 +49,25 @@ describe('Test /mcda', () => {
             expect(resultTechIds.length).eq(2)
         });
     });
-    describe("ecosystem services are calculated", () => {
-        it('the key score_multifunctionality is generated in the result', async () => {
-            let result = await mcda({techIds: ["WW"]})
-            expect(result[0]).to.have.property('score_multifunctionality');
-        });
-        it('score is between 0 and 1', async () => {
-            let result = await mcda({techIds: ["WW"]})
-            expect(result[0].score_multifunctionality).to.be.within(0,1)
-        })
-    });
-    describe("operation and manteinance is calculated", () => {
-        it('the key score_operation is generated in the result', async () => {
-            let result = await mcda({techIds: ["WW"]})
-            expect(result[0]).to.have.property('score_operation');
-        });
-        it('score is between 0 and 1', async () => {
-            let result = await mcda({techIds: ["WW"]})
+    describe('environmental impacts is calculated', () => {
+
+        it('the key score_env_impact is generated and between 0 and 1', async () => {
+            let selection = await findNBS({})
+            let result = await mcda({techs: selection})
+            expect(result[10]).to.have.property('score_env_impact')
             result.forEach(tech => {
-                expect(tech.score_operation).to.be.within(0,1)
+                expect(tech.score_env_impact).to.be.within(0, 1)
             })
+        });
+    });
+    describe("biodiversity is calculated", () => {
+        it('the key score_biodiversity is generated in the result', async () => {
+            let result = await mcda({techIds: ["WW"]})
+            expect(result[0]).to.have.property('score_biodiversity');
+        });
+        it('score is between 0 and 1', async () => {
+            let result = await mcda({techIds: ["WW"]})
+            expect(result[0].score_biodiversity).to.be.within(0,1)
         })
     });
     describe("space requirements are calculated", () => {
@@ -101,7 +101,7 @@ describe('Test /mcda', () => {
         });
     });
     describe("space requirements are calculated without water information", () => {
-        it("for treatment, when m2_pe is bigger, the score is smallar", async () => {
+        it("for treatment, when m2_pe is bigger, the score is smaller", async () => {
             let result = await mcda({techIds: ["WW", "A_HA"]})
             expect(result[0].m2_pe_tropical).lt(result[1].m2_pe_tropical)
             expect(result[0].score_space_requirements).gt(result[1].score_space_requirements)
@@ -112,33 +112,59 @@ describe('Test /mcda', () => {
             expect(result[1].score_space_requirements).lt(result[0].score_space_requirements)
         });
     })
-    describe('environmental impacts is calculated', () => {
-
-        it('the key score_env_impact is generated and between 0 and 1', async () => {
-            let selection = await findNBS({})
-            let result = await mcda({techs: selection})
-            expect(result[10]).to.have.property('score_env_impact')
-            result.forEach(tech => {
-                expect(tech.score_env_impact).to.be.within(0, 1)
-            })
+    describe("social benefits are calculated", () => {
+        it('the key score_social_benefits is generated in the result', async () => {
+            let result = await mcda({techIds: ["WW"]})
+            expect(result[0]).to.have.property('score_social_benefits');
         });
+        it('score is between 0 and 1', async () => {
+            let result = await mcda({techIds: ["WW", "PP_GR"]})
+            result.forEach(tech => {
+                expect(tech.score_social_benefits).to.be.within(0,1)
+            })
+        })
     });
-    describe('cost is calculated', () => {
-        it('the key cost is generated and is the same regardless the surface', async () => {
+    describe("circularity is calculated", () => {
+        it('the key score_circularity is generated in the result', async () => {
+            let result = await mcda({techIds: ["WW"]})
+            expect(result[0]).to.have.property('score_circularity');
+        });
+        it('score is between 0 and 1', async () => {
+            let result = await mcda({techIds: ["A_FWS", "BS_RG"]})
+            result.forEach(tech => {
+                expect(tech.score_circularity).to.be.within(0,1)
+            })
+        })
+    });
+    describe("operation and manteinance is calculated", () => {
+        it('the key score_operation is generated in the result', async () => {
+            let result = await mcda({techIds: ["WW"]})
+            expect(result[0]).to.have.property('score_operation');
+        });
+        it('score is between 0 and 1', async () => {
+            let result = await mcda({techIds: ["WW"]})
+            result.forEach(tech => {
+                expect(tech.score_operation).to.be.within(0,1)
+            })
+        })
+    });
+
+    describe('capex is calculated', () => {
+        it('the key capex is generated and is the same regardless the surface', async () => {
             let df_with_surface = await findNBS({techIds: ["TR_TR", "BS_BS", "GR_IR"],
                 waterType: "rain_water", cumRain: 300, catchmentArea: 1000, duration: 2, infiltrationSoils: "clay"})
             let result_with_surface = await mcda({techs: df_with_surface})
             console.log("df_with_surfacd", df_with_surface)
             // console.log(result_with_surface[0])
-            expect(result_with_surface[0]).to.have.property('estimated_cost_mean')
+            expect(result_with_surface[0]).to.have.property('estimated_capex_mean')
 
             let result_without_surface = await mcda({techIds: ["TR_TR", "BS_BS", "GR_IR"], waterType: "rain_water"})
-            result_with_surface.map(e => expect(e).to.have.property('score_cost'))
-            result_without_surface.map(e => expect(e).to.have.property('score_cost'))
+            result_with_surface.map(e => expect(e).to.have.property('score_capex'))
+            result_without_surface.map(e => expect(e).to.have.property('score_capex'))
 
-            expect(result_with_surface[0].score_cost).to.almost.equal(result_without_surface[0].score_cost)
-            expect(result_with_surface[1].score_cost).to.almost.equal(result_without_surface[1].score_cost)
-            expect(result_with_surface[2].score_cost).to.almost.equal(result_without_surface[2].score_cost)
+            expect(result_with_surface[0].score_capex).to.almost.equal(result_without_surface[0].score_capex)
+            expect(result_with_surface[1].score_capex).to.almost.equal(result_without_surface[1].score_capex)
+            expect(result_with_surface[2].score_capex).to.almost.equal(result_without_surface[2].score_capex)
         });
 
         it('surface is only deleted if it was added', async () => {
@@ -153,18 +179,18 @@ describe('Test /mcda', () => {
             result_without_surface.map(e => expect(e).to.not.have.property('vertical_surface_low'))
 
         });
-        it('score_cost is between 0 and 1', async () => {
+        it('score_capex is between 0 and 1', async () => {
             let result= await mcda({techIds: ["TR_TR", "BS_BS", "GR_IR"], waterType: "rain_water"})
-            result.map(e => expect(e.score_cost).to.be.within(0, 1))
+            result.map(e => expect(e.score_capex).to.be.within(0, 1))
         });
-        it('score_cost is larger if cost_high * surface is smaller', async () => {
+        it('score_capex is larger if capex_high * surface is smaller', async () => {
             let df_with_surface = await findNBS({techIds: ["TR_TR", "BS_BS", "GR_IR"],
                 waterType: "rain_water", cumRain: 300, catchmentArea: 1000, duration: 2})
             let result = await mcda({techs: df_with_surface})
-            let cost_1 = result[1].cost_high * result[1].surface_mean
-            let cost_2 = result[0].cost_high * result[0].surface_mean
-            expect(cost_1).gt(cost_2)
-            expect(result[1].score_cost).lt(result[0].score_cost)
+            let capex_1 = result[1].capex_high * result[1].surface_mean
+            let capex_2 = result[0].capex_high * result[0].surface_mean
+            expect(capex_1).gt(capex_2)
+            expect(result[1].score_capex).lt(result[0].score_capex)
         });
     });
     describe('Weights are calculated correctly', () => {
@@ -175,7 +201,7 @@ describe('Test /mcda', () => {
             expect(result.wMultifunctionality).lt(result.wOperation)
         });
         it('if all scores are 0 weights are converted to 1', () => {
-           let result = calculateWeights({wEnvImpact: 0, wMultifunctionality: 0, wOperation: 0, wSpaceRequirements: 0, wCost: 0})
+           let result = calculateWeights({wEnvImpact: 0, wMultifunctionality: 0, wOperation: 0, wSpaceRequirements: 0, wCapex: 0})
            expect(Object.values(result).filter(e => e == 0.20).length).eq(Object.values(result).length)
         });
         it('percentages are correct', () => {
@@ -192,7 +218,7 @@ describe('Test /mcda', () => {
                 wMultifunctionality: 0.24,
                 wSpaceRequirements: 0.2,
                 wEnvImpact: 0.2,
-                wCost: 0.2
+                wCapex: 0.2
             }
             let wDefault = 0.2
             result_treatment.forEach(tech => {
@@ -204,7 +230,7 @@ describe('Test /mcda', () => {
             result_swm.forEach(tech => {
                 expect(tech.weighted_multifunctionality).eq(tech.score_multifunctionality * weights.wMultifunctionality)
                 expect(tech.weighted_operation).eq(tech.score_operation * weights.wOperation)
-                expect(tech.weighted_cost).eq(tech.score_cost * weights.wCost)
+                expect(tech.weighted_capex).eq(tech.score_capex * weights.wCapex)
                 expect(tech.weighted_space_requirements).eq(tech.score_space_requirements * weights.wSpaceRequirements)
             });
             result2.forEach(tech => {
